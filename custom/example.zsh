9bf8cabf81d702afe12e29386daa0a15c822dbeb
Duan Fang <dfang@iMac.local>
2013-01-10 19:28:50 +0800

    Moved platform_specific.zsh and add some alias

--- custom/example.zsh
+++ custom/example.zsh
@@ -3,3 +3,11 @@
 #
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
+
+function sp {
+  if [ -f $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh ]; then
+    source $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh
+  elif [ -f $ZSH/plugins/$plugin/$plugin.plugin.zsh ]; then
+    source $ZSH/plugins/$plugin/$plugin.plugin.zsh
+  fi
+}
